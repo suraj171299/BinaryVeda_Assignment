0e93801ce04df519e1379d6754f93327f0193e01
suraj171299 <surajpadalkar1999@gmail.com>
2024-10-08 07:01:28 +0530

    Added get route

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,10 @@ const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use('/v1/api/user', userRoutes)
 
+app.get('/', (req, res) => {
+    res.send('Welcome to BinaryVeda Assignment')
+})
+
 
 app.listen(PORT, () => {
     connectDB();
